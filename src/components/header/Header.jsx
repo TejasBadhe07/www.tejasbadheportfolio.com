256a3f8c88ce0a9b18548988fafc00589e896981
TejasBadhe07 <tbadhe75@gmail.com>
2024-03-08 19:46:32 +0530

    Profile Updates

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/images/Photo1.jpeg'
+import ME from '../../assets/images/Tejas.jpeg'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
