8394b7884c1b2b1896c59b3ab215bb2f171cd346
TejasBadhe07 <tbadhe75@gmail.com>
2024-03-12 13:43:55 +0530

    update tag

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,21 +6,23 @@ import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <header>
-      <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Tejas</h1>
-        <h5 className ="text-light"> Fullstack Developer</h5>
-        <CTA />
-        <HeaderSocials/>
-        
-        <div className='me'>
-          <img src={ME} alt="me" />            {/* IMAGE OF FIRST PAGE */} 
-        </div>
+    <section id='home'>
+      <header>
+        <div className="container header__container">
+          <h5>Hello I'm</h5>
+          <h1>Tejasss</h1>
+          <h5 className ="text-light"> Fullstack Developer</h5>
+          <CTA />
+          <HeaderSocials/>
+          
+          <div className='me'>
+            <img src={ME} alt="me" />            {/* IMAGE OF FIRST PAGE */} 
+          </div>
 
-        <a href="#contact" className='scroll__down'>Scroll Down</a>
-      </div>
-    </header>
+          <a href="#contact" className='scroll__down'>Scroll Down</a>
+        </div>
+      </header>
+      </section>
   )
 }
 
