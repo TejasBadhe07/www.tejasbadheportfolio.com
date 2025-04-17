70a2472f1e5631697efea1199436ccb778b4b561
TejasBadhe07 <tbadhe75@gmail.com>
2025-04-17 23:30:39 +0530

    Fixed navigation bar removed the social media icons from the nav bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,9 +4,6 @@ import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {BiMessageSquareDetail} from 'react-icons/bi'
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
-import {BsInstagram} from 'react-icons/bs'
 import { motion } from 'framer-motion'
 
 const Nav = () => {
@@ -72,35 +69,6 @@ const Nav = () => {
       >
         <BiMessageSquareDetail/>
       </motion.a>
-      <div className="nav__socials">
-        <motion.a 
-          href="https://www.linkedin.com/in/tejasbadhe07/" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95 }}
-        >
-          <BsLinkedin/>
-        </motion.a>
-        <motion.a 
-          href="https://github.com/TejasBadhe07" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95 }}
-        >
-          <FaGithub/>
-        </motion.a>
-        <motion.a 
-          href="https://www.instagram.com/tejasbadhe07" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95 }}
-        >
-          <BsInstagram/>
-        </motion.a>
-      </div>
     </motion.nav>
   )
 }
